72e803904ad79029b571709a4cdf6d9a777f560d
singhsivansh12 <singhshivansh12@gmail.com>
2021-06-22 00:04:09 +0530

    each atch details added

--- IPL/IPL_scraping_project/cricinfo_scraping.js
+++ IPL/IPL_scraping_project/cricinfo_scraping.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+const base_url = "https://www.espncricinfo.com/";
+
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
 
 
@@ -10,7 +12,10 @@ function main_func(error, response, html){
     if(error){
         console.log(error);
     }
-    get_view_all(html);
+    const view_all_html = get_view_all(html);
+
+    // console.log(view_all_html);
+    request(view_all_html, get_all_matches);
 }
 
 
@@ -19,5 +24,25 @@ function get_view_all(html){
 
     const view_all = $('[data-hover="View All Results"]');
 
-    console.log(view_all.attr('href'));
+    return (base_url+view_all.attr('href'));
+}
+
+
+const get_all_matches = (error, response, html)=>{
+    if(error){
+        console.log(error);
+    }
+
+    const $ = cheerio.load(html);
+
+    const main_container = $(".card.content-block.league-scores-container");
+
+    const match_block_array = main_container.find(".match-score-block");
+
+    console.log(match_block_array.length);
+
+    for(let i=0; i<match_block_array.length; i++){
+        const match_block =  $(match_block_array[i]).find(".match-info-link-FIXTURES");
+        console.log($(match_block[0]).attr('href'), "\n");
+    }
 }
