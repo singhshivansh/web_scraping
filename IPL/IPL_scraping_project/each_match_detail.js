3aa29d6f198036d6fec2b59c8c2899cd4d788687
singhsivansh12 <singhshivansh12@gmail.com>
2021-06-23 01:10:15 +0530

    IPL project completed

--- IPL/IPL_scraping_project/each_match_detail.js
+++ IPL/IPL_scraping_project/each_match_detail.js
@@ -1,9 +1,11 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const xlsx    = require('xlsx');
 
 const fs = require('fs');
+const path = require('path');
 
-// const url = "https://www.espncricinfo.com//series/ipl-2020-21-1210595/delhi-capitals-vs-kolkata-knight-riders-16th-match-1216515/full-scorecard";
+const url = "https://www.espncricinfo.com//series/ipl-2020-21-1210595/delhi-capitals-vs-kolkata-knight-riders-16th-match-1216515/full-scorecard";
 
 function main_fn(url){
     request(url, (error, response, html)=>{
@@ -26,6 +28,9 @@ function get_details(html){
     team1 = get_team_name(match_blocks[0]);
     team2 = get_team_name(match_blocks[1]);
 
+    make_folder(team1);
+    make_folder(team2);
+
     // console.log(team1, team2);
 
     const venue  = get_venue(html);
@@ -38,7 +43,7 @@ function get_details(html){
         const team_name = get_team_name(match_blocks[i]);
         // console.log(team_name);
         make_folder(team_name);
-        batsman(match_blocks[i], team_name);
+        batsman(match_blocks[i], team_name, venue, date, result);
     }
 }
 
@@ -75,6 +80,12 @@ const get_result = (html) => {
     return main_block.find('.status-text').text();
 }
 
+const parentFolder = () => {
+    const filePath = path.join(__dirname, 'IPL_Teams');
+    if(!fs.existsSync(filePath))
+        fs.mkdirSync(filePath);
+}
+
 //creating folders on team name
 const make_folder = (team_name) => {
     if(!fs.existsSync(`./IPL_Teams/${team_name}`))
@@ -85,10 +96,12 @@ const make_folder = (team_name) => {
         })
 }
 
-const batsman = (html, team_name)=>{
+
+const batsman = (html, team_name, venue, date, result)=>{
     const $ = cheerio.load(html);
 
     const batsman_team = team_name == team1 ? team1 : team2;
+    const opponent_team = team_name == team1 ? team2 : team1;
 
     const batsman_table = $('.table.batsman tbody tr');
 
@@ -97,12 +110,62 @@ const batsman = (html, team_name)=>{
             const batsman_cell = $(batsman_table[i]).find('td');
 
             //teamname // name //venue //date //opponent //result //runs //balls //fours //sixes //strike rate
-            // console.log($(batsman_cell[0]).text(), $(batsman_cell[2]).text())
-            console.log($(batsman_cell[0]).text(), batsman_team);
+
+            
+            const playerData = {
+                "Team Name" : batsman_team,
+                "Name" : $(batsman_cell[0]).text().trim(),
+                "Venue" : venue.trim(),
+                "Date" : date.trim(),
+                "Opponent Team" : opponent_team.trim(),
+                "Result" : result.trim(),
+                "Runs" : $(batsman_cell[2]).text().trim(),
+                "Balls Faced" : $(batsman_cell[3]).text().trim(),
+                "Fours" : $(batsman_cell[5]).text().trim(),
+                "Sixes" : $(batsman_cell[6]).text().trim(),
+                "Strike Rate" : $(batsman_cell[7]).text().trim()
+            }
+
+            processPlayer(playerData, team_name, $(batsman_cell[0]).text().trim());
         }
     }
     
 }
 
 
-main_fn(url);
+const processPlayer = (playerData, teamName, playerName)=>{
+    parentFolder();
+
+    const filePath = path.join(__dirname, 'IPL_Teams', teamName, playerName + ".xlsx");
+    let content = excelReader(filePath, playerName);
+
+    content.push(playerData);
+
+    excelWriter(filePath, content, playerName);
+}
+
+
+const excelWriter = (filePath, data, sheetName)=>{
+    let newWB = xlsx.utils.book_new();
+    let newWS = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
+    xlsx.writeFile(newWB, filePath);
+}
+
+const excelReader = (filePath, sheetName)=>{
+    if(fs.existsSync(filePath) == false)
+        return [];
+    let wb = xlsx.readFile(filePath);
+    let excelData = wb.Sheets[sheetName];
+    let ans = xlsx.utils.sheet_to_json(excelData);
+
+    return ans;
+}
+
+
+// main_fn(url);
+
+
+module.exports = {
+    main_fn : main_fn
+}
