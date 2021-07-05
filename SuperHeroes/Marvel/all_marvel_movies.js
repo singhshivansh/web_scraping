9f785694a9d4c0e0dd103d459e41e6074a3dd337
singhsivansh12 <singhshivansh12@gmail.com>
2021-07-06 01:48:10 +0530

    changes

--- SuperHeroes/Marvel/all_marvel_movies.js
+++ SuperHeroes/Marvel/all_marvel_movies.js
@@ -1,7 +1,9 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const { compress } = require('pdfkit');
 
 const url = 'https://www.imdb.com/list/ls021778931/';
+const base_url = 'https://www.imdb.com/';
 
 request(url, main);
 
@@ -9,6 +11,7 @@ function main(error, response, html){
     if(error)
         console.log(error);
     
+    // console.log(response);
     const $ = cheerio.load(html);
 
     const movie_list = $('.lister-list').find('.lister-item-content');
@@ -19,9 +22,36 @@ function main(error, response, html){
     }
 }
 
+let movies = []
+
 function get_movie_details(movie_element){
-    const $ = cheerio.load(movie_element);
-    const movie_name = $('.lister-item-header').text().split('.')[1].split('(')[0].trim();
-    const year       = $('.lister-item-year.text-muted.unbold').text().split(' ');
-    console.log(year);
+    // console.log("pp");
+    const $             = cheerio.load(movie_element);
+    const movie_name    = $('.lister-item-header').find('a').text();
+    let movie_url       =  $('.lister-item-header').find('a').attr('href');
+    const year1         = $('.lister-item-year.text-muted.unbold').text().split(' ');
+    const year          = year1[year1.length-1];
+    let rating          = $('.ipl-rating-widget .ipl-rating-star__rating').text().slice(0,4);
+    if (rating.slice(2,4) == 'Ra')
+        rating = rating[0] + '.00';
+    const directors_and_stars = $($('.text-muted.text-small')[1]).text().split('|');
+    let director = directors_and_stars[0].trim().split(':');
+    
+    let stars = directors_and_stars[1].trim().split(":");
+  
+    stars = stars.splice(1);
+    director  = director.splice(1);
+    
+    const movie_json = {
+        "name" : movie_name,
+        "year" : year,
+        "rating" : rating,
+        director : director,
+        stars : stars
+    }
+    movies.push(movie_json);
+    // console.log();
+    console.log(movie_json);
 }
+
+console.log(movies);
